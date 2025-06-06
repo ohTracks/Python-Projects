323fd5952505046fe27cd7bf0be3a673ae17288e
Gabriel <gabrielbastosgalo@gmail.com>
2025-06-06 00:27:05 -0300

    coke_machine.py challenge done

---
+++ coke_machine.py
@@ -0,0 +1,18 @@
+
+d = 50
+
+print(f"Amount due: {d}")
+
+c = int(input("We only accept coins of 25, 10 and 5 cents.\nInsert coins: "))
+
+if c != 25 and c != 10 and c != 5:
+    c = int(input(f"Amount due: {d}\nPlease insert a valid coin: "))
+        
+    
+while c < d:
+    print(f"Amount due: {d - c}")
+    c += int(input("Insert coins: "))
+
+print(f"Change owed: {c - d}")
+
+exit(0)
